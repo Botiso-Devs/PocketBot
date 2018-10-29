84960d2c9a42aae60d35939281cdcb6bbe28bcc5
Botiso-Devs <44485369+Botiso-Devs@users.noreply.github.com>
2018-10-29 03:50:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client()
 bot.on("ready", () => {
   console.log("Bot Online")
   
-bot.user.setActivity("PocketIslands | -help", {type: "PLAYING"})
+bot.user.setActivity("TwinFactions | -help", {type: "PLAYING"})
 });
   
 bot.login(process.env.TOKEN)
